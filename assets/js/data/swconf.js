6afb76a386a41d06aa22025f0e32329abc25b166
naffan2014 <naffan2012@icloud.com>
2026-07-14 04:02:15 +0000

    publish _site at 2026-07-14T04:02:15Z from docs@68c13a7

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1783776697',resources: [
+    cacheName: 'chirpy-1784001734',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
